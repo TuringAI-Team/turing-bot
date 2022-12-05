2b417fde197dd02f07443ef20b9572d117d09290
Mrlolpro <leonardoollero14@gmail.com>
2022-12-05 16:34:00 +0100

    feat: add env.sample fix: removed supabase url

--- src/modules/supabase.js
+++ src/modules/supabase.js
@@ -1,6 +1,6 @@
 const { createClient } = require("@supabase/supabase-js");
 
-const supabaseUrl = "https://angbrsnflkxfamwrvabk.supabase.co";
+const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
