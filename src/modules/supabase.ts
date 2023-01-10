645ca5dcc42da7ac9d15dd0ffbf165102f1953d8
MrlolDev <65485999+MrlolDev@users.noreply.github.com>
2023-01-10 06:53:58 +0100

    fix: supabase import

--- src/modules/supabase.ts
+++ src/modules/supabase.ts
@@ -1,4 +1,4 @@
-const { createClient } = require("@supabase/supabase-js");
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
