203a88793b41b345926014c34ed61c38f37ed549
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-01-29 16:33:58 +0100

    feat: premium

--- src/modules/premium.ts
+++ src/modules/premium.ts
@@ -1,4 +1,3 @@
-import { EndMethod } from "discord.js-pages";
 import supabase from "./supabase.js";
 import ms from "ms";
 
@@ -24,7 +23,45 @@ export async function isPremium(id: string) {
   return false;
 }
 
-export async function makeItPremium(id: string, method: string) {
+export async function activateKey(key: string, id: string) {
+  let { data: keyD, error } = await supabase
+    .from("keys")
+    .select("*")
+
+    // Filters
+    .eq("key", key)
+    .eq("claimed", false);
+  if (keyD && keyD[0]) {
+    var duration = keyD[0].duration;
+    if (duration == "1m") duration = "30d";
+    await makeItPremium(id, "key", duration);
+    await supabase
+      .from("keys")
+      .update({
+        claimed: true,
+        claimedAt: Date.now(),
+        claimedBy: id,
+        duration: duration,
+      })
+
+      // Filters
+      .eq("key", key)
+      .eq("claimed", false);
+    return {
+      message: "Premium activated successfully",
+    };
+  } else {
+    return {
+      error: "Invalid key",
+    };
+  }
+}
+
+export async function makeItPremium(
+  id: string,
+  method: string,
+  duration: string
+) {
   let { data: premium, error } = await supabase
     .from("premium")
     .select("*")
@@ -32,30 +69,30 @@ export async function makeItPremium(id: string, method: string) {
     // Filters
     .eq("id", id);
   if (premium && premium[0]) {
-    await renew(id, method);
+    await renew(id, method, duration);
   } else {
-    await create(id, method);
+    await create(id, method, duration);
   }
 }
 
-async function renew(id: string, method: string) {
+async function renew(id: string, method: string, duration: string) {
   const { data, error } = await supabase
     .from("premium")
     .update({
       renewed_at: Date.now(),
       method: method,
-      expires_at: Date.now() + ms("30d"),
+      expires_at: Date.now() + ms(duration),
     })
     .eq("id", id);
 }
 
-async function create(id: string, method: string) {
+async function create(id: string, method: string, duration: string) {
   const { data, error } = await supabase.from("premium").insert([
     {
       id: id,
       renewed_at: Date.now(),
       method: method,
-      expires_at: Date.now() + ms("30d"),
+      expires_at: Date.now() + ms(duration),
     },
   ]);
 }
